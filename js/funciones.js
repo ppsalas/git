434c79fa668ed8df17cae8f2f473846e714e9bb5
ppsalas <jo.salaso@duocuc.cl>
2021-05-13 03:47:25 -0400

    gg

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    $("#basic-form").validate({
+    $("#registrarsee").validate({
         rules: {
             name: {
                 required: true,
                 minlength: 3
             },
             password: {
-                required: true,               
+                required: true,
                 minlength: 4
             },
             email: {
@@ -16,3 +16,45 @@ $(document).ready(function () {
         }
     });
 });
+
+
+$(document).ready(function () {
+    $("#iniciarsesion").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 3
+            },
+            password: {
+                required: true,
+                minlength: 4
+            },
+            email: {
+                required: true,
+                email: true
+            },
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    $("#subirobraa").validate({
+      rules: {
+        name : {
+          required: true,
+          minlength: 3
+        },
+        precio: {
+          required: true,
+          number: true,
+          min: 1
+        },
+        email: {
+          required: true,
+          email: true
+        },
+      }
+    });
+  });
+
