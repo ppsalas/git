8e9d91b671afbdc6bbc54445c967a32e1df868fd
Jose <82198272+ppsalas@users.noreply.github.com>
2021-07-07 14:40:08 -0400

    api get

--- TestDjango/rest_producto/views.py
+++ TestDjango/rest_producto/views.py
@@ -9,19 +9,21 @@ from .serializers import ProductoSerializer
 @csrf_exempt
 @api_view(['GET','POST'])
 def lista_productos(request):
-
-    if request.method == 'GET':
-        producto = Producto.objects.all()
-        serializer = ProductoSerializer(producto, many=True)
+    
+    '''
+    LISTA TODOS LOS PRODUCTO
+    '''
+    if (request.method=='GET'):
+        producto=Producto.objects.all()
+        serializer=ProductoSerializer(producto, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+    elif (request.method=='POST'):
         data = JSONParser().parse(request)
         serializer = ProductoSerializer(data=data)
-        if serializer.is_valid():
+        if (serializer.is_valid()):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 
