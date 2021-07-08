f2def14700e9cb970d1d8774565f54089090d61c
Jose <82198272+ppsalas@users.noreply.github.com>
2021-07-08 05:45:21 -0400

    z

--- TestDjango/core/static/core/js/script.js
+++ TestDjango/core/static/core/js/script.js
@@ -1,17 +1,18 @@
-$(document).ready(function () {
-    //alert('paso1')
-    $.getJSON('127.0.0.1:8000/api/lista-productos', function (data) {
+
+$(document).ready(function() {
+    $.getJSON('127.0.0.1:8000/api/lista_productos', function(data) {
         console.log(data)
-        var productos = data;
+        var productos = data;        
         $('#cod_producto').html(productos.cod_producto);
         $('#nombre').html(productos.nombre);
         $('#precio').html(productos.precio);
         $('#stock').html(productos.stock);
         $('#descripcion').html(productos.descripcion);
         $('#foto').attr("src", productos.foto);
-    }).fail(function () {
+    }).fail(function() {
         console.log('Error al consumir la API!');
     });
+
 });
 
 
